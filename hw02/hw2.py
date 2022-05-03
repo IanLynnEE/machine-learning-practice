a52b0a02b5f4d60892f693a07950320c1a0b46da
IanLynnEE <kmes50311@gmail.com>
2022-05-03 15:17:06 +0800

    Update hw02/hw2.py Finish draft for hw2.

--- hw02/hw2.py
+++ hw02/hw2.py
@@ -9,54 +9,103 @@ import math
 import scipy.stats
 import argparse
 
-def phi_gbf(x, O1, O2, scale=None, center=None):
+
+def _phi_gbf(x, O1, O2, scale=None, center=None):
+    '''
+    Input:
+        x      : ndarray with size (length of data, 2 features)
+        O1     : int
+        O2     : int
+        scale  : (optional) ndarray with size (2, )
+        center : (optional) ndarray with size (2, O1*O2)
+    output:
+        phi    : ndarray with size (length of data, O1*O2)
+        scale  : ndarray with size (2, )
+        center : ndarray with size (2, O1*O2)
+    '''
     phi = np.zeros([np.shape(x)[0], O1*O2])
     if scale is None:
         scale = (np.max(x,axis=0) - np.min(x,axis=0)) / np.array([O1-1, O2-1])
         center = np.zeros([2, O1*O2])
-        for j in range(O2):
-            for i in range(O1):
-                k = O2*i+j
-                center[:, k] = scale * np.array([i, j]) + np.min(x, axis=0)
-                dis = x - center[:, k] 
-                phi_before_exp = np.square(dis) / (2 * np.square(scale)).T
-                phi[:, k] = np.exp(-np.sum(phi_before_exp, axis=1))
-        return phi, scale, center
-    for j in range(O2):
         for i in range(O1):
-            k = O2*i+j
-            dis = x - center[:, k] 
-            phi_before_exp = np.square(dis) / (2 * np.square(scale)).T
-            phi[:, k] = np.exp(-np.sum(phi_before_exp, axis=1))
-    return phi 
+            for j in range(O2):
+                center[:, O2*i+j] = scale * np.array([i,j]) + np.min(x,axis=0)
+    for i in range(O1):
+        for j in range(O2):
+            dis = x - center[:, O2*i+j]      # Somehow this is okey.
+            dis_scaled = np.square(dis) / (2 * np.square(scale)).T
+            phi[:, O2*i+j] = np.exp(-np.sum(dis_scaled, axis=1)) # -(a+b)
+    return phi, scale, center
 
 
-def BLR(train_data, test_data_feature, O1=5, O2=5): 
+def _fit_BLR(x, y, alpha=1e-5, beta=1e-5, max_iter=100):
     '''
-    output: ndarray with size (length of test_data, )
+    Input:
+        x        : ndarray with size (length of data, number of features)
+        y        : ndarray with size (length of data, )
+        alpha    : (optional) float, initial alpha
+        beta     : (optional) float, initial beta
+        max_iter : (optional) int
+    output:
+        m_N      : ndarray with size (length of data, number of features)
     '''
-    return
-    return y_BLRprediction 
+    for i in range(max_iter):
+        # Update S_N, m_N, gamma from previous alpha, beta.
+        S_N_inv = alpha * np.eye(x.shape[1]) + beta * x.T.dot(x)
+        S_N = np.linalg.inv(S_N_inv)
+        m_N = beta * S_N.dot(x.T).dot(y)
+        eigenvalues = np.linalg.eigvalsh(x.T.dot(x)) * beta
+        gamma = np.sum(eigenvalues / (eigenvalues + alpha))
+        # Update alpha, beta from above.
+        alpha_new = gamma / np.sum(m_N.T.dot(m_N))
+        beta_new = (np.shape(x)[0] - gamma) / np.sum(np.square(y - x.dot(m_N)))
+        # Check convergence.
+        if np.isclose(alpha, alpha_new) and np.isclose(beta, beta_new):
+            print(f'Converge after {i + 1} iterations.')
+            return m_N
+        alpha = alpha_new; beta = beta_new
+    print(f'Stop after {max_iter} iterations.')
+    return m_N
 
 
-def MLR(x, xt, O1=5, O2=5):  
+def BLR(x, xt, O1=5, O2=5): 
     '''
     input:
-        x    : training data with label
-        xt   : test_data_feature
-    output: ndarray with size (length of test_data, )
+        x  : ndarray with size (length of train_data, 3 features + 1 label)
+        xt : ndarray with size (length of test_data, 3 features)
+        O1 : int
+        O2 : int
+    output:
+        prediction : ndarray with size (length of test_data, )
     '''
     phi_train = np.zeros([np.shape(x)[0], O1*O2+2])
     phi_train[:, O1*O2] = x[:, 2]
     phi_train[:, O1*O2+1] = 1
-    phi_train[:, :O1*O2], scale, center = phi_gbf(x[:, :2], O1, O2)
+    phi_train[:, :O1*O2], scale, center = _phi_gbf(x[:, :2], O1, O2)
+    m_N = _fit_BLR(phi_train, x[:, 3])
     phi_test = np.zeros([np.shape(xt)[0], O1*O2+2])
     phi_test[:, O1*O2] = xt[:, 2]
     phi_test[:, O1*O2+1] = 1
-    phi_test[:, :O1*O2] = phi_gbf(xt[:, :2], O1, O2, scale, center)
-    w = np.dot(np.linalg.pinv(phi_train), x[:, 3])
-    y_MLLSprediction = np.dot(phi_test, w)
-    return y_MLLSprediction 
+    phi_test[:, :O1*O2], _, _ = _phi_gbf(xt[:, :2], O1, O2, scale, center) 
+    # m_N = _fit_BLR(x[:, :2], x[:, 3])
+    # return xt[:, :2].dot(m_N)
+    return phi_test.dot(m_N)
+
+
+def MLR(x, xt, O1=5, O2=5):  
+    '''
+    input & output: same as above.
+    '''
+    phi_train = np.zeros([np.shape(x)[0], O1*O2+2])
+    phi_train[:, O1*O2] = x[:, 2]
+    phi_train[:, O1*O2+1] = 1
+    phi_train[:, :O1*O2], scale, center = _phi_gbf(x[:, :2], O1, O2)
+    w = np.linalg.pinv(phi_train).dot(x[:, 3])
+    phi_test = np.zeros([np.shape(xt)[0], O1*O2+2])
+    phi_test[:, O1*O2] = xt[:, 2]
+    phi_test[:, O1*O2+1] = 1
+    phi_test[:, :O1*O2], _, _ = _phi_gbf(xt[:, :2], O1, O2, scale, center)
+    return phi_test.dot(w)
 
 
 def CalMSE(data, prediction):
@@ -82,7 +131,6 @@ def main():
     predict_BLR = BLR(data_train, data_test_feature, O1=O_1, O2=O_2)
     predict_MLR = MLR(data_train, data_test_feature, O1=O_1, O2=O_2)
 
-    print(f'MSE of MLR = {CalMSE(predict_MLR, data_test_label)}')
     print('MSE of BLR = {e1}, MSE of MLR= {e2}.'.format(
         e1=CalMSE(predict_BLR, data_test_label), 
         e2=CalMSE(predict_MLR, data_test_label)
