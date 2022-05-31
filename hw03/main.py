dbfad90c1ec98bff70fbc148df3a6b37ea7ab410
IanLynnEE <66598373+IanLynnEE@users.noreply.github.com>
2022-05-31 08:00:52 +0800

    Update hw03/main.py

--- hw03/main.py
+++ hw03/main.py
@@ -8,7 +8,7 @@ import seaborn as sns
 from sklearn.decomposition import PCA
 from sklearn.metrics import ConfusionMatrixDisplay, accuracy_score
 
-
+from model import TwoLayersClassifier
 
 def load_data(path: str) -> tuple[np.ndarray]:
     # All images MUST have same dimension.
@@ -39,72 +39,6 @@ def pre_processing(x: np.ndarray, xt: np.ndarray) -> tuple[np.ndarray]:
     return x_pca, xt_pca
 
 
-# TODO
-# 1. Confirm softmax and it's impact.
-# 2. SGD
-class TwoLayersClassifier:
-    def __init__(self, n_h=6, *, rate=0.1, atol=1e-5, max_iters=10000):
-        self.n_h = n_h
-        self.rate = rate
-        self.atol = atol
-        self.max_iters = max_iters
-
-    # Sigmoid as Activation Function
-    def sigma(self, x):
-        return 1 / (1 + np.exp(-x))
-    def d_sigma(self, x):
-        return self.sigma(x) * (1 - self.sigma(x))
-    
-    def softmax(self, x):
-        exps = np.exp(x - x.max(axis=1, keepdims=True))
-        return exps / np.sum(exps, axis=1, keepdims=True)
-
-    def fit(self, x, labels):
-        N, M = np.shape(x)
-        n_out = np.max(labels)
-        # Make one-hot label.
-        y = np.zeros((N, n_out))
-        for i in range(N):
-            y[i, labels[i] - 1] = 1
-        # Initial value of weight
-        self.w1 = np.random.rand(self.n_h, M + 1) * np.sqrt(1/self.n_h)
-        self.w2 = np.random.rand(n_out, self.n_h + 1) * np.sqrt(1/n_out)
-        training_loss = []
-        for i in range(self.max_iters):
-            self.forward(x)
-            training_loss.append(self.cross_entropy_loss(y))
-            grad = self.backprop(y)
-            self.w1 -= grad[0] * self.rate
-            self.w2 -= grad[1] * self.rate
-            if grad[0].max() < self.atol and grad[1].max() < self.atol:
-                break
-        plt.plot(np.array(training_loss))
-        plt.show()
-
-    def predict(self, xt):
-        self.forward(xt)
-        return self.a3.argmax(axis=1) + 1
-
-    def forward(self, x):
-        self.a1 = np.insert(x, 0, 1, axis=1)
-        self.z2 = self.a1 @ self.w1.T
-        self.a2 = np.insert(self.sigma(self.z2), 0, 1, axis=1)
-        self.z3 = self.a2 @ self.w2.T
-        self.a3 = self.softmax(self.z3)
-    
-    def cross_entropy_loss(self, y):
-        return -np.sum(y * np.log(self.a3)) / y.shape[0]
-
-    def backprop(self, y):
-        delta3 = self.a3 - y
-        delta2 = delta3 @ self.w2[:, 1:] * self.d_sigma(self.z2)
-        w2_grad = delta3.T @ self.a2
-        w1_grad = delta2.T @ self.a1
-        w2_grad /= y.shape[0]
-        w1_grad /= y.shape[0]
-        return w1_grad, w2_grad
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--layer', type=int, default=2)
@@ -117,7 +51,8 @@ if __name__ == '__main__':
     xt, yt = load_data(args.test_path)
     x,  xt = pre_processing(x, xt)
 
-    clf = TwoLayersClassifier(args.hidden_node)
+    if args.layer == 2:
+        clf = TwoLayersClassifier(args.hidden_node)
     clf.fit(x, y)
     yp = clf.predict(xt)
     print(accuracy_score(yt, yp))
