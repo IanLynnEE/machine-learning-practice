9a5a90ee6421d348a5326a26adccccb262fc6040
IanLynnEE <66598373+IanLynnEE@users.noreply.github.com>
2022-06-01 23:17:29 +0800

    Update hw03/model.py 1. Rename `self.z` to `self.act`. 2. Rename `self.a` to `self.unit`. 3. Rename NNClassifier_batch to NNClassifierBatch 4. Move `make_one_hot()` to outside. 5. Remove matplotlib.

--- hw03/model.py
+++ hw03/model.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 
 def sigmoid(x, derivative=False):
@@ -14,6 +13,11 @@ def softmax(x):
     exps = np.exp(x - x.max())
     return exps / np.sum(exps)
 
+def make_one_hot(labels):
+    y = np.zeros((len(labels), np.max(labels)))
+    for i in range(len(labels)):
+        y[i, labels[i] - 1] = 1
+    return y
 
 
 class NNClassifier:
@@ -28,23 +32,18 @@ class NNClassifier:
         self.w = [theta for _ in range(n_l)]
         self.w[0] = np.random.rand(n_h, n_f + 1) * np.sqrt(1/n_h)
         self.w[n_l-1] = np.random.rand(n_o, n_h + 1) * np.sqrt(1/n_o)
-        self.z = [0 for _ in range(n_l+1)]
-        self.a = [0 for _ in range(n_l+1)]
+        self.act = [0 for _ in range(n_l+1)]
+        self.unit = [0 for _ in range(n_l+1)]
     
     def cross_entropy(self, yt, yp):
         return -np.sum(yt * np.log(yp)) / yt.shape[0]
 
-    def make_one_hot(self, labels):
-        y = np.zeros((len(labels), np.max(labels)))
-        for i in range(len(labels)):
-            y[i, labels[i] - 1] = 1
-        return y
 
     def fit(self, x, labels, x_val=None, label_val=None):
-        y = self.make_one_hot(labels)
+        y = make_one_hot(labels)
         yp = np.ones_like(y)
         if x_val is not None:
-            y_val = self.make_one_hot(label_val)
+            y_val = make_one_hot(label_val)
             yp_val = np.ones_like(y_val)
         training_loss = np.zeros(self.epochs)
         valid_loss = np.zeros(self.epochs)
@@ -55,12 +54,12 @@ class NNClassifier:
                 grad = self.backprop(y[rand])
                 for i in range(0, self.n_l):
                     self.w[i] -= grad[i] * self.rate
-                yp[rand] = self.z[self.n_l]
+                yp[rand] = self.unit[self.n_l]
             training_loss[k] = self.cross_entropy(y, yp)
             if x_val is not None:
                 for j in range(len(x_val)):
                     self.forward(x_val[j])
-                    yp_val[j] = self.z[self.n_l]
+                    yp_val[j] = self.unit[self.n_l]
                 valid_loss[k] = self.cross_entropy(y_val, yp_val)
         if x_val is not None:
             return training_loss, valid_loss
@@ -70,74 +69,70 @@ class NNClassifier:
         yp = np.zeros(xt.shape[0])
         for i in range(xt.shape[0]):
             self.forward(xt[i])
-            yp[i] = self.a[self.n_l].argmax() + 1
+            yp[i] = self.unit[self.n_l].argmax() + 1
         return yp
 
     def forward(self, x):
-        self.a[0] = np.insert(x, 0, 1)
+        self.unit[0] = np.insert(x, 0, 1)
         for i in range(1, self.n_l + 1):
-            self.z[i] = self.w[i-1] @ self.a[i-1]
-            self.a[i] = np.insert(sigmoid(self.z[i]), 0, 1)
-        self.a[self.n_l] = softmax(self.z[self.n_l])
+            self.act[i] = self.w[i-1] @ self.unit[i-1]
+            self.unit[i] = np.insert(sigmoid(self.act[i]), 0, 1)
+        self.unit[self.n_l] = softmax(self.act[self.n_l])
 
     def backprop(self, y):
         dw = [0 for _ in range(self.n_l)]
-        delta = self.a[self.n_l] - y
+        delta = self.unit[self.n_l] - y
         for i in range(self.n_l-1, 0, -1):
-            dw[i] = np.outer(delta, self.a[i])
-            delta = self.w[i][:, 1:].T @ delta * sigmoid(self.z[i], True)
-        dw[0] = np.outer(delta, self.a[0])
+            dw[i] = np.outer(delta, self.unit[i])
+            delta = self.w[i][:, 1:].T @ delta * sigmoid(self.act[i], True)
+        dw[0] = np.outer(delta, self.unit[0])
         return dw
 
 
-class NNClassifier_batch:
-    def __init__(self, n_o, n_f, *, n_l=3, n_h=6, rate=0.1, max_iters=10000):
+class NNClassifierBatch:
+    def __init__(self, n_o, n_f, *, n_l=3, n_h=6, rate=0.1, epochs=10000):
         self.n_l = n_l
         self.rate = rate
-        self.max_iters = max_iters
+        self.epochs = epochs
         # Initial value of weight
         theta = np.random.rand(n_h, n_h + 1) * np.sqrt(1/n_h)
         self.w = [theta for _ in range(n_l)]
         self.w[0] = np.random.rand(n_h, n_f + 1) * np.sqrt(1/n_h)
         self.w[n_l-1] = np.random.rand(n_o, n_h + 1) * np.sqrt(1/n_o)
-        self.z = [0 for _ in range(n_l+1)]
-        self.a = [0 for _ in range(n_l+1)]
+        self.act = [0 for _ in range(n_l+1)]
+        self.unit = [0 for _ in range(n_l+1)]
     
     def cross_entropy(self, y):
-        return -np.sum(y * np.log(self.a[self.n_l])) / y.shape[0]
+        return -np.sum(y * np.log(self.unit[self.n_l])) / y.shape[0]
 
     def fit(self, x, labels):
-        # Make one-hot label.
-        y = np.zeros((len(labels), np.max(labels)))
-        for i in range(len(labels)):
-            y[i, labels[i] - 1] = 1
-
-        training_loss = []
-        self.a[0] = np.insert(x, 0, 1, axis=1)
-        for i in range(self.max_iters):
+        y = make_one_hot(labels)
+        training_loss = np.zeros(self.epochs)
+        self.unit[0] = np.insert(x, 0, 1, axis=1)
+        for j in range(self.epochs):
             self.forward()
             grad = self.backprop(y)
             for i in range(0, self.n_l):
                 self.w[i] -= grad[i] * self.rate
-            training_loss.append(self.cross_entropy(y))
-        plt.plot(np.array(training_loss))
+            training_loss[j] = self.cross_entropy(y)
+        return training_loss
 
     def predict(self, xt):
-        self.a[0] = np.insert(xt, 0, 1, axis=1)
+        self.unit[0] = np.insert(xt, 0, 1, axis=1)
         self.forward()
-        return self.a[self.n_l].argmax(axis=1) + 1
+        return self.unit[self.n_l].argmax(axis=1) + 1
 
     def forward(self):
         for i in range(1, self.n_l + 1):
-            self.z[i] = self.a[i-1] @ self.w[i-1].T
-            self.a[i] = np.insert(sigmoid(self.z[i]), 0, 1, axis=1)
-        self.a[self.n_l] = softmax(self.z[self.n_l])
+            self.act[i] = self.unit[i-1] @ self.w[i-1].T
+            self.unit[i] = np.insert(sigmoid(self.act[i]), 0, 1, axis=1)
+        self.unit[self.n_l] = softmax(self.act[self.n_l])
 
     def backprop(self, y):
         dw = [0 for _ in range(self.n_l)]
-        delta = self.a[self.n_l] - y
+        delta = self.unit[self.n_l] - y
         for i in range(self.n_l-1, 0, -1):
-            dw[i] = delta.T @ self.a[i] / y.shape[0]
-            delta = delta @ self.w[i][:, 1:] * sigmoid(self.z[i], True)
-        dw[0] = delta.T @ self.a[0] / y.shape[0]
+            dw[i] = delta.T @ self.unit[i] / y.shape[0]
+            delta = delta @ self.w[i][:, 1:] * sigmoid(self.act[i], True)
+        dw[0] = delta.T @ self.unit[0] / y.shape[0]
         return dw
